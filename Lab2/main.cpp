d5134858e7fa892fbb97eece1f72b0918ae6dbed
7dywk <illkryt321@gmail.com>
2026-09-21 15:51:47 +0300

    Lab 2.1 code commit

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,14 +1,36 @@
-//
-//  main.cpp
-//  Lab2
-//
-//  Created by Illia on 21.09.2026.
-//
+
 
 #include <iostream>
+#include <cmath> // Бібліотека для математичних функцій (sin, cos, tan)
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+using namespace std;
+
+int main()
+{
+    // 1. Оголошення змінних
+    double alpha; // вхідний параметр (кут у радіанах)
+    double z1;    // результат обчислення 1-го виразу
+    double z2;    // результат обчислення 2-го виразу
+
+    // 2. Введення даних
+    cout << "Введіть значення кута alpha (у радіанах): ";
+    cin >> alpha;
+
+    // 3. Обчислення z1 за першою формулою
+    // Для зручності читання розіб'ємо дріб на чисельник та знаменник
+    double numerator = sin(alpha) + sin(5 * alpha) - sin(3 * alpha);
+    double denominator = cos(alpha) - cos(3 * alpha) + cos(5 * alpha);
+    z1 = numerator / denominator;
+
+    // 4. Обчислення z2 за другою формулою (tg 3a)
+    z2 = tan(3 * alpha);
+
+    // 5. Виведення результатів
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0; // Успішне завершення програми
 }
+
+
