75a60cae2a066cab03ca859437a1049ac51a1647
7dywk <illkryt321@gmail.com>
2026-09-21 15:42:00 +0300

    Initial Commit

---
+++ Lab2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab2
+//
+//  Created by Illia on 21.09.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
